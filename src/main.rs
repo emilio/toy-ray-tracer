c62a31c1eadb5ac050a7204f9d79335c87888345
Emilio Cobos Álvarez <emilio@crisal.io>
2017-01-13 00:53:49 +0100

    wip, seems to do something.
    I got really stuck for a while for doing this:
    
      gl::buffer_data(gl::ARRAY_BUFFER, &[
          -1.0, ...
      ], gl::STATIC_DRAW);
    
    Rust (now obviously) interprets them as being `f64`s, and I acted as-is they
    were `float`s, so I was fucked.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,134 @@
 extern crate euclid;
 extern crate glutin;
-extern crate gl;
+extern crate gleam;
 extern crate tracer;
 
-use euclid::{Size2D, TypedPoint3D};
+use euclid::Size2D;
+use gleam::gl;
 use tracer::WorldPosition;
 
+const VERTEX_SHADER_SOURCE: &'static [u8] = b"\
+#version 150                                                                   \n\
+in vec2 vPosition;                                                             \n\
+out vec2 fPosition;                                                            \n\
+void main() {                                                                  \n\
+    fPosition = vPosition;                                                     \n\
+    gl_Position = vec4(fPosition, 0.0, 1.0);                                   \n\
+}";
+
+
+const FRAGMENT_SHADER_SOURCE: &'static [u8] = b"\
+#version 150                                                                   \n\
+uniform sampler2D uTexture;                                                    \n\
+in vec2 fPosition;                                                             \n\
+out vec4 oFragColor;                                                           \n\
+void main() {                                                                  \n\
+    oFragColor = texture2D(uTexture, fPosition * 0.5 + 0.5);                   \n\
+    // oFragColor = vec4(fPosition * 0.5 + 0.5, 0.0, 1.0);                   \n\
+}";
+
 fn main() {
-    let window = glutin::Window::new().unwrap();
+    let window = glutin::WindowBuilder::new()
+        .with_title("Hello world!")
+        .build()
+        .unwrap();
 
     unsafe { window.make_current() };
 
-    unsafe {
-        gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+    gl::clear_color(0.0, 1.0, 0.0, 1.0);
 
-        gl::ClearColor(0.0, 1.0, 0.0, 1.0);
-    }
+    let gl_version = gl::get_string(gl::VERSION);
+    let gl_renderer = gl::get_string(gl::RENDERER);
+
+    println!("OpenGL version {}, {}", gl_version, gl_renderer);
+    println!("hidpi factor: {}", window.hidpi_factor());
 
     let mut scene = tracer::Scene::new(Size2D::new(800, 800),
                                        WorldPosition::new(0.0, 0.0, -100.),
                                        WorldPosition::new(0.0, 0.0, -100.));
-    let sphere = tracer::ObjectKind::Sphere { radius: 10. };
-    let sphere = tracer::Object::new(sphere, WorldPosition::new(0., 0., 0.));
+    let sphere = tracer::ObjectKind::Sphere { radius: 100. };
+    let sphere = tracer::Object::new(sphere, WorldPosition::new(400., 400., 0.));
     scene.add_object(sphere);
 
-    let sphere = tracer::ObjectKind::Sphere { radius: 12. };
-    let sphere = tracer::Object::new(sphere, WorldPosition::new(0., 5., 0.));
+    let sphere = tracer::ObjectKind::Sphere { radius: 50. };
+    let sphere = tracer::Object::new(sphere, WorldPosition::new(500., 500., 0.));
     scene.add_object(sphere);
+    let buffer = scene.draw();
+
+    let size = scene.size();
+    let texture = gl::gen_textures(1)[0];
+    gl::bind_texture(gl::TEXTURE_2D, texture);
+
+    gl::tex_image_2d(gl::TEXTURE_2D, 0, gl::RGBA as gl::GLint,
+                     size.width as i32, size.height as i32, 0,
+                     gl::RGBA, gl::UNSIGNED_BYTE, Some(&buffer));
+    gl::tex_parameter_i(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as gl::GLint);
+    gl::tex_parameter_i(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as gl::GLint);
+    gl::tex_parameter_i(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as gl::GLint);
+    gl::tex_parameter_i(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as gl::GLint);
+    gl::tex_parameter_i(gl::TEXTURE_2D, gl::TEXTURE_WRAP_R, gl::CLAMP_TO_EDGE as gl::GLint);
+
+    let program = gl::create_program();
+    let vshader = gl::create_shader(gl::VERTEX_SHADER);
+
+    gl::shader_source(vshader, &[VERTEX_SHADER_SOURCE]);
+    gl::compile_shader(vshader);
+
+    println!("VShader: {}", gl::get_shader_info_log(vshader));
+
+    let fshader = gl::create_shader(gl::FRAGMENT_SHADER);
+    gl::shader_source(fshader, &[FRAGMENT_SHADER_SOURCE]);
+    gl::compile_shader(fshader);
+
+    println!("FShader: {}", gl::get_shader_info_log(fshader));
+
+    gl::attach_shader(program, vshader);
+    gl::attach_shader(program, fshader);
+
+    gl::link_program(program);
+    println!("program: {}", gl::get_program_info_log(program));
+    gl::validate_program(program);
+    println!("program: {}", gl::get_program_info_log(program));
+
+    gl::use_program(program);
+
+
+    let vao = gl::gen_vertex_arrays(1)[0];
+    gl::bind_vertex_array(vao);
+
+    let gl_buffer = gl::gen_buffers(1)[0];
+    gl::bind_buffer(gl::ARRAY_BUFFER, gl_buffer);
+
+    let data: [f32; 12] = [
+        -1.0, 1.0,
+         1.0, 1.0,
+        -1.0, -1.0,
+        -1.0, -1.0,
+         1.0, 1.0,
+         1.0, -1.0,
+    ];
+
+    gl::buffer_data(gl::ARRAY_BUFFER, &data, gl::STATIC_DRAW);
+
+    let pos = gl::get_attrib_location(program, "vPosition");
+    gl::enable_vertex_attrib_array(pos as gl::GLuint);
+    gl::vertex_attrib_pointer(0, 2, gl::FLOAT, false, 0, 0);
+    let tex_uniform = gl::get_uniform_location(program, "uTexture");
+    gl::uniform_1i(tex_uniform, 0);
+    gl::active_texture(gl::TEXTURE0);
 
     for event in window.wait_events() {
-        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
+        gl::clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+
+        gl::draw_arrays(gl::TRIANGLES, 0, 6);
         window.swap_buffers();
 
         match event {
             glutin::Event::Closed => break,
-            _ => ()
+            _ => {},
         }
     }
+
+    gl::delete_textures(&[texture]);
 }
--- tracer/src/lib.rs
+++ tracer/src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(field_init_shorthand)]
+#![feature(field_init_shorthand, const_fn)]
 
 extern crate euclid;
 
@@ -16,6 +16,7 @@ pub struct Object {
     position: WorldPosition,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Color {
     r: u8,
     g: u8,
@@ -24,7 +25,7 @@ pub struct Color {
 }
 
 impl Color {
-    fn new(r: u8,
+    const fn new(r: u8,
            g: u8,
            b: u8,
            a: u8) -> Self {
@@ -45,30 +46,90 @@ impl Object {
     }
 }
 
+#[derive(Debug)]
 struct Ray {
     position: WorldPosition,
     direction: Point3D<f32>,
     strength: f32,
 }
 
-struct Intersection {
-    position: WorldPosition,
-    normal: Point3D<f32>,
+fn length<U>(p: TypedPoint3D<f32, U>) -> f32 {
+    (p.x * p.x + p.y * p.y + p.z * p.z).sqrt()
+}
+
+fn scale<U>(v: TypedPoint3D<f32, U>, factor: f32) -> TypedPoint3D<f32, U> {
+    TypedPoint3D::new(v.x * factor, v.y * factor, v.z * factor)
+}
+
+fn to_world_units<U>(v: TypedPoint3D<f32, U>) -> WorldPosition {
+    TypedPoint3D::new(v.x, v.y, v.z)
+}
+
+fn reflect<U>(dir: TypedPoint3D<f32, U>,
+              normal: TypedPoint3D<f32, U>)
+              -> TypedPoint3D<f32, U> {
+    dir - scale(normal, normal.dot(dir) * 2.0)
 }
 
 impl Ray {
     fn new(position: WorldPosition,
-               direction: Point3D<f32>,
-               strength: f32) -> Self {
+           direction: Point3D<f32>,
+           strength: f32) -> Self {
         Ray { position, direction, strength }
     }
 
-    fn intersects_with(&self, object: &Object) -> Option<Intersection> {
-        // TODO(emilio)
-        None
+    // Tries to calculate the ray intersection between this ray and a given
+    // object, and returns the reflected ray, if any.
+    fn intersects_with(&self, object: &Object) -> Option<Ray> {
+        match object.kind {
+            ObjectKind::Sphere  { radius } => {
+                // https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
+                //
+                // this is |l \dot (o - c)|
+                let b =
+                    self.direction.dot((self.position - object.position).to_untyped());
+                if !b.is_finite() {
+                    return None;
+                }
+
+                // This is the value under the square root.
+                let v = b * b -
+                    length(self.position - object.position).powf(2.0)
+                    + radius.powf(2.0);
+
+                if v < 0.0 {
+                    return None;
+                }
+
+                // TODO(emilio): This should be in the material of the object.
+                const OBJECT_REFLECTIVENESS: f32 = 0.5;
+
+                // Tangent intersection -> same ray
+                if v == 0.0 {
+                    let distance = -b;
+                    let position = self.position + to_world_units(scale(self.direction, distance));
+                    return Some(Ray::new(position,
+                                         self.direction,
+                                         self.strength * OBJECT_REFLECTIVENESS));
+                }
+
+                let v = v.sqrt();
+                let distance = (-b + v).min(-b - v);
+                if !distance.is_finite() {
+                    return None;
+                }
+
+                let position = self.position + to_world_units(scale(self.direction, distance));
+                let normal = position - object.position;
+                let new_direction = reflect(self.direction, normal.to_untyped());
+
+                Some(Ray::new(position,
+                              new_direction,
+                              self.strength * OBJECT_REFLECTIVENESS))
+            }
+        }
     }
 }
-
 
 pub struct Scene {
     size: Size2D<usize>,
@@ -85,6 +146,33 @@ impl Scene {
         Scene { size, camera_position, light_position, objects: vec![], }
     }
 
+    pub fn size(&self) -> Size2D<usize> {
+        self.size
+    }
+
+    fn get_color_at(&self,
+                    framebuffer: &[u8],
+                    x: usize,
+                    y: usize) -> Color {
+        let slice = &framebuffer[x * 4 + y * self.size.width * 4..];
+
+        Color::new(slice[0], slice[1], slice[2], slice[3])
+    }
+
+
+    fn set_color_at(&self,
+                    framebuffer: &mut [u8],
+                    x: usize,
+                    y: usize,
+                    color: Color) {
+        let slice = &mut framebuffer[x * 4 + y * self.size.width * 4..];
+        slice[0] = color.r;
+        slice[1] = color.g;
+        slice[2] = color.b;
+        slice[3] = color.a;
+    }
+
+
     pub fn add_object(&mut self, object: Object) {
         self.objects.push(object)
     }
@@ -95,6 +183,10 @@ impl Scene {
         // of stuff.
         let mut framebuffer = vec![0u8; self.size.width * self.size.height * 4];
         let mut z_buffer = vec![0f32; self.size.width * self.size.height];
+
+        self.set_color_at(&mut framebuffer, self.size.width - 1, self.size.height - 1, Color::new(0, 0, 0, 255));
+        assert_eq!(framebuffer[framebuffer.len() - 1], 255);
+
 
         for x in 0..self.size.width {
             for y in 0..self.size.height {
@@ -110,6 +202,53 @@ impl Scene {
                      y: usize,
                      framebuffer: &mut [u8],
                      z_buffer: &mut [f32]) {
-        // TODO.
+        const MAX_REFLECTIONS: usize = 10;
+
+        let mut current_ray = Ray::new(
+            WorldPosition::new(x as f32, y as f32, -1000.0),
+            Point3D::new(0.0, 0.0, 1.0),
+            1.0);
+
+        let mut current_intersection: Option<(Ray, &Object)> = None;
+        let mut current_reflection = 0;
+
+        while current_reflection <= MAX_REFLECTIONS && current_ray.strength != 0.0 {
+            for object in &self.objects {
+                let intersection = match current_ray.intersects_with(object) {
+                    None => continue,
+                    Some(i) => i,
+                };
+
+                let should_override_current = match current_intersection {
+                    Some((ref current, _)) => intersection.position.z < current.position.z,
+                    None => true,
+                };
+
+                if should_override_current {
+                    current_intersection = Some((intersection, object));
+                }
+            }
+
+            match current_intersection.take() {
+                Some((current, _object)) => {
+                    // TODO(emilio): get this from the object.
+                    const OBJECT_COLOR: Color = Color::new(255, 255, 0, 255);
+                    println!("Current: {:?}", current);
+                    let color = self.get_color_at(framebuffer, x, y);
+                    self.set_color_at(framebuffer, x, y,
+                                 Color::new(
+                                     (color.r as f32 + OBJECT_COLOR.r as f32 * current.strength) as u8,
+                                     (color.g as f32 + OBJECT_COLOR.g as f32 * current.strength) as u8,
+                                     (color.b as f32 + OBJECT_COLOR.b as f32 * current.strength) as u8,
+                                     (color.a as f32 + OBJECT_COLOR.a as f32 * current.strength) as u8)
+                                 );
+
+                    current_ray = current;
+                }
+                None => break,
+            }
+
+            current_reflection += 1;
+        }
     }
 }
